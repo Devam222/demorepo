1bda00da11c8c46a79f74f3eb9716ac243ee40c6
deva <deva@edureka.co>
2019-12-18 19:08:02 +0530

    Second File added.

---
+++ sample2.java
@@ -0,0 +1,7 @@
+class Demo{
+	public static void main(string[] args){
+		system.out.println("This is a sample second program.");
+		system.out.println("Git is very easy to operate");
+		system.out.println("Git commands are part of chapter 1");
+	}
+}
