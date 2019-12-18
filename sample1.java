cd4a664a12bedf85f6f03dd02cce3f8c7ebb0b96
Devam222 <59006477+Devam222@users.noreply.github.com>
2019-12-17 22:01:04 -0800

    Update sample1.java

--- sample1.java
+++ sample1.java
@@ -1,6 +1,6 @@
 class Demo{
 	public static void main(string[] args){
 		system.out.println("This is a sample program.");
-		system.out.println("Git is very easy to operate");
+		system.out.println("Git is very easy to operate and widely used tool");
 	}
 }
